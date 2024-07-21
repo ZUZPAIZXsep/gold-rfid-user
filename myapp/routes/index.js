d504764b625fcff0c7df74181ad8c89427a1d158
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2024-07-21 15:57:41 +0700

    update

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -101,6 +101,33 @@ router.get('/', async (req, res, next) => {
     let condition = {};
     const golds = await Goldtagscount.find(condition);
 
+    const dataUrl = 'http://www.thaigold.info/RealTimeDataV2/gtdata_.txt';
+
+        const response = await axios.get(dataUrl);
+        const data = response.data;
+
+        // console.log('Data from API:', data);
+
+        const pricePerGram = parseFloat(data[5]?.bid); // ราคาเสนอซื้อของทองคำ 96.5%
+
+        if (isNaN(pricePerGram)) {
+            console.error('pricePerGram is not a valid number:', pricePerGram);
+            res.status(500).send('Invalid price data from API');
+            return;
+        }
+
+        // คำนวณราคาทองคำตามน้ำหนักต่างๆ
+        const prices = {
+            halfSalung: pricePerGram * 3.81 / 15.244 / 2,
+            oneSalung: pricePerGram * 3.81 / 15.244,
+            twoSalung: pricePerGram * 3.81 * 2 / 15.244,
+            oneBaht: pricePerGram,
+            twoBaht: pricePerGram * 2,
+            threeBaht: pricePerGram * 3
+        };
+
+        const updateTime = data[0]?.ask; // เวลาที่แสดงในดัชนีที่ [0] และคีย์ 'ask'
+
     // เรียงข้อมูลตามลำดับถาด
       golds.sort((a, b) => {
       const trayOrder = {
@@ -115,7 +142,12 @@ router.get('/', async (req, res, next) => {
       return trayOrder[a.gold_tray] - trayOrder[b.gold_tray];
     });
 
-    res.render('index', { golds: golds, dayjs: dayjs, currentUrl: req.originalUrl });
+    res.render('index', { 
+      golds: golds, 
+      dayjs: dayjs, 
+      currentUrl: req.originalUrl, 
+      prices, 
+      updateTime});
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
